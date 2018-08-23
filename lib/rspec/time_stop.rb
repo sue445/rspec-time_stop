93c5867a58a72968fd8450d0130e493ed38c8112
sue445 <sue445@sue445.net>
2018-08-23 23:17:29 +0900

    bundle gem rspec-time_stop --mit

---
+++ lib/rspec/time_stop.rb
@@ -0,0 +1,7 @@
+require "rspec/time_stop/version"
+
+module Rspec
+  module TimeStop
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/time_stop/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module TimeStop
+    VERSION = "0.1.0"
+  end
+end
